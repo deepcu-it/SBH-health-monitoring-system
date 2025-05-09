eb238f621b0317d8fd411803b11527f125556932
deepcu-it <deepghosal445@gmail.com>
2025-05-10 00:01:02 +0530

    Update Firebase configuration and modify .gitignore; change subproject commit to dirty state

--- health_monitor_copy/lib/main.dart
+++ health_monitor_copy/lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:health_monitor/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyDsf2zVZBtGmHu4h7H_bUu8-gGl8KHH7dM",
-        authDomain: "health-4f948.firebaseapp.com",
-        databaseURL: "https://health-4f948-default-rtdb.firebaseio.com",
-        projectId: "health-4f948",
-        storageBucket: "health-4f948.firebasestorage.app",
-        messagingSenderId: "1027615118164",
-        appId: "1:1027615118164:web:ae1a933561fc3420cdeec1",
-        measurementId: "G-L3T8C5GSE9"),
+    options: FirebaseConfig.options,
   );
   runApp(const MyApp());
 }
